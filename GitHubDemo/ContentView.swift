c2dd85ffd8a58c1d870417e497b79543b0968731
pradeepiOS <pradeep.iostech@gmail.com>
2021-02-17 14:17:41 +0530

    Initial Commit

---
+++ GitHubDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubDemo
+//
+//  Created by Pradeep's Macbook on 17/02/21.
+//  Copyright © 2021 Motiv Ate Fitness. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
