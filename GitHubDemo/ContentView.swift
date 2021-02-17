852966c7f6df97400f1661cfd73ef7da59fcfe66
pradeepiOS <pradeep.iostech@gmail.com>
2021-02-17 15:14:03 +0530

    For conflicting

--- GitHubDemo/ContentView.swift
+++ GitHubDemo/ContentView.swift
@@ -12,8 +12,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Hello, World!")
-            Text("From development- xcode 12")
-            Text("Xcode 12")
+            Text("From - xcode 12")
+            Text("conflict")
         }
     }
 }
